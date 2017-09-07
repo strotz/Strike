d69474af55085c99bc56b0824670d1eacf108faf
Alexei Strots <strotz@gmail.com>
2017-09-07 15:14:06 -0700

    Jenkins psm1 code format

--- Scripts/Jenkins.psm1
+++ Scripts/Jenkins.psm1
@@ -1,137 +1,132 @@
 
 Function Generate-NodeFile ($slaveName, $slaveDescription, $slaveLabel, $jenkinsLogin, $properties) {
 
-   # TODO: ask?
-   Import-Module $PSScriptRoot\ConfigLoad.psm1 -Force
-   $jenkinsHome = $global:config.JenkinsLocation
+    # TODO: ask?
+    Import-Module $PSScriptRoot\ConfigLoad.psm1 -Force
+    $jenkinsHome = $global:config.JenkinsLocation
 
 
-   $xml = "<slave>`n"
-   $xml += "<name>$slaveName</name>`n"
-   $xml += "<description>$slaveDescription</description>`n"
-   $xml += "<remoteFS>$jenkinsHome</remoteFS>`n"
-   $xml += "<numExecutors>1</numExecutors>`n"
+    $xml = "<slave>`n"
+    $xml += "<name>$slaveName</name>`n"
+    $xml += "<description>$slaveDescription</description>`n"
+    $xml += "<remoteFS>$jenkinsHome</remoteFS>`n"
+    $xml += "<numExecutors>1</numExecutors>`n"
 
-   if ($slaveLabel) {
-      $xml += "<mode>EXCLUSIVE</mode>`n"
-      $xml += "<label>$slaveLabel</label>`n"
-   }
-   else
-   {
-      Write-Host "Skipping slave label" 		
-   }
+    if ($slaveLabel) {
+        $xml += "<mode>EXCLUSIVE</mode>`n"
+        $xml += "<label>$slaveLabel</label>`n"
+    }
+    else {
+        Write-Host "Skipping slave label" 		
+    }
 
-   if ($properties) {
-      $xml += "<nodeProperties>`n"
-      $xml += "<hudson.slaves.EnvironmentVariablesNodeProperty>`n"
-      $xml += "<envVars serialization=`"custom`">`n"
-      $xml += "<unserializable-parents/>`n"
-      $xml += "<tree-map>`n"
-      $xml += "<default><comparator class=`"hudson.util.CaseInsensitiveComparator`"/></default>`n"
+    if ($properties) {
+        $xml += "<nodeProperties>`n"
+        $xml += "<hudson.slaves.EnvironmentVariablesNodeProperty>`n"
+        $xml += "<envVars serialization=`"custom`">`n"
+        $xml += "<unserializable-parents/>`n"
+        $xml += "<tree-map>`n"
+        $xml += "<default><comparator class=`"hudson.util.CaseInsensitiveComparator`"/></default>`n"
 
-      $xml += "<int>$($properties.count)</int>`n"
-      foreach ($key in $properties.Keys) {
-         $xml += "<string>$($key)</string>`n"
-         $xml += "<string>$($properties.$key)</string>`n" 
-      }
+        $xml += "<int>$($properties.count)</int>`n"
+        foreach ($key in $properties.Keys) {
+            $xml += "<string>$($key)</string>`n"
+            $xml += "<string>$($properties.$key)</string>`n" 
+        }
 
-      $xml += "</tree-map>`n"
-      $xml += "</envVars>`n"
-      $xml += "</hudson.slaves.EnvironmentVariablesNodeProperty>`n"
-      $xml += "</nodeProperties>`n"
-   }
-   else
-   { 
-      Write-Host "Skipping properties" 		
-   }
+        $xml += "</tree-map>`n"
+        $xml += "</envVars>`n"
+        $xml += "</hudson.slaves.EnvironmentVariablesNodeProperty>`n"
+        $xml += "</nodeProperties>`n"
+    }
+    else { 
+        Write-Host "Skipping properties" 		
+    }
 
-   $xml += "<retentionStrategy class=`"hudson.slaves.RetentionStrategy`$Always`"/>`n"
-   $xml += "<launcher class=`"hudson.slaves.JNLPLauncher`"/>`n"
-   $xml += "<userId>$jenkinsLogin</userId>`n"
-   $xml += "</slave>`n" 
-   return $xml
+    $xml += "<retentionStrategy class=`"hudson.slaves.RetentionStrategy`$Always`"/>`n"
+    $xml += "<launcher class=`"hudson.slaves.JNLPLauncher`"/>`n"
+    $xml += "<userId>$jenkinsLogin</userId>`n"
+    $xml += "</slave>`n" 
+    return $xml
 }
 
-Function Register-Slave 
-{
-param(
-    $slaveName,
-    $slaveDescription,
-    $slaveLabel
-)
+Function Register-Slave {
+    param(
+        $slaveName,
+        $slaveDescription,
+        $slaveLabel
+    )
 
-Write-Host "Registering as jenkins slave..."
+    Write-Host "Registering as jenkins slave..."
 
-if (!$slaveName) {
-    Write-Host -fore red "Slave name must be specified" 
-}
+    if (!$slaveName) {
+        Write-Host -fore red "Slave name must be specified" 
+    }
 
-Import-Module $PSScriptRoot\ConfigLoad.psm1 -Force
-Import-Module $PSScriptRoot\World.psm1
+    Import-Module $PSScriptRoot\ConfigLoad.psm1 -Force
+    Import-Module $PSScriptRoot\World.psm1
 
-#
-# Confirm install location
-#
+    #
+    # Confirm install location
+    #
 
-$location = Resolve-InstallLocation
-$jenkinsCliLocation = "$location\jenkins\"
-$jenkinsCli = "$($jenkinsCliLocation)jenkins-cli.jar"
+    $location = Resolve-InstallLocation
+    $jenkinsCliLocation = "$location\jenkins\"
+    $jenkinsCli = "$($jenkinsCliLocation)jenkins-cli.jar"
 
-if (-not (Test-Path $jenkinsCliLocation)) 
-{ 
-    $_ = New-Item -ItemType Directory -Force -Path $jenkinsCliLocation
-}
+    if (-not (Test-Path $jenkinsCliLocation)) { 
+        $_ = New-Item -ItemType Directory -Force -Path $jenkinsCliLocation
+    }
 
-$jenkins = $global:config.JenkinsServerUrl
-Write-Host "Now let's connect to Jenkins server ($jenkins) and register slave" 
-$wc = New-Object System.Net.WebClient
+    $jenkins = $global:config.JenkinsServerUrl
+    Write-Host "Now let's connect to Jenkins server ($jenkins) and register slave" 
+    $wc = New-Object System.Net.WebClient
 
-if (-not (Test-Path $jenkinsCli)) 
-{ 
-    $jenkinsCliUrl = "$($jenkins)jnlpJars/jenkins-cli.jar"
-    Write-Host "Downloading Jenkins client from $jenkinsCliUrl to $jenkinsCli"
-    $start_time = Get-Date
-    $wc.DownloadFile($jenkinsCliUrl, $jenkinsCli)
-    Write-Host "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
-}
+    if (-not (Test-Path $jenkinsCli)) { 
+        $jenkinsCliUrl = "$($jenkins)jnlpJars/jenkins-cli.jar"
+        Write-Host "Downloading Jenkins client from $jenkinsCliUrl to $jenkinsCli"
+        $start_time = Get-Date
+        $wc.DownloadFile($jenkinsCliUrl, $jenkinsCli)
+        Write-Host "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+    }
 
-#
-# TODO: current assumption that java binary located at $($global:config.InstallLocation)\jre\bin\java.exe 
-#
+    #
+    # TODO: current assumption that java binary located at $($global:config.InstallLocation)\jre\bin\java.exe 
+    #
 
-$java = "$($global:config.InstallLocation)\jre\bin\java.exe"
+    $java = "$($global:config.InstallLocation)\jre\bin\java.exe"
 
-$jenkinsLogin = Resolve-JenkinsLogin
-$jenkinsPassword = Resolve-JenkinsPassword
+    $jenkinsLogin = Resolve-JenkinsLogin
+    $jenkinsPassword = Resolve-JenkinsPassword
 
-# TODO: unify to use Invoke-Command
-$run = Start-Process $java -ArgumentList '-jar',$jenkinsCli,'-s',$jenkins,'login','--username',$jenkinsLogin,'--password',$jenkinsPassword -NoNewWindow -PassThru
-$run.WaitForExit()
+    # TODO: unify to use Invoke-Command
+    $run = Start-Process $java -ArgumentList '-jar', $jenkinsCli, '-s', $jenkins, 'login', '--username', $jenkinsLogin, '--password', $jenkinsPassword -NoNewWindow -PassThru
+    $run.WaitForExit()
 
-$properties = $global:config.NodeProperties
-$xml = Generate-NodeFile -SlaveName $slaveName -SlaveDescription $slaveDescription -SlaveLabel $slaveLabel -JenkinsLogin $jenkinsLogin -Properties $properties
+    $properties = $global:config.NodeProperties
+    $xml = Generate-NodeFile -SlaveName $slaveName -SlaveDescription $slaveDescription -SlaveLabel $slaveLabel -JenkinsLogin $jenkinsLogin -Properties $properties
 
-# TODO: unify to use Invoke-Command
-$arguments = "-jar",$jenkinsCli,"-s",$jenkins,"create-node",$slaveName
-$xml | & $java $arguments 
+    # TODO: unify to use Invoke-Command
+    $arguments = "-jar", $jenkinsCli, "-s", $jenkins, "create-node", $slaveName
+    $xml | & $java $arguments 
 
-$groovyScript = "$PSScriptRoot\readslave.groovy"
-$arguments = '-jar',$jenkinsCli,'-s',$jenkins,'groovy',$groovyScript,$slaveName
-$result = Invoke-Command -CommandTitle  'readslave' -CommandPath  $java  -CommandArguments $arguments
-$secret = $result.stdout.TrimEnd()
+    $groovyScript = "$PSScriptRoot\readslave.groovy"
+    $arguments = '-jar', $jenkinsCli, '-s', $jenkins, 'groovy', $groovyScript, $slaveName
+    $result = Invoke-Command -CommandTitle  'readslave' -CommandPath  $java  -CommandArguments $arguments
+    $secret = $result.stdout.TrimEnd()
 
-# TODO: download slave.jar to $jenkinsCliLocation C:\automation\jenkins
+    # TODO: download slave.jar to $jenkinsCliLocation C:\automation\jenkins
 
-#
-# Create start_slave.cmd
-#
-$startCmd = ""
-$startCmd += "set NAME=$slaveName`r`n"
-$startCmd += "set JSERVER=$jenkins`r`n"
-$startCmd += "set SECRET=$secret`r`n"
-$startCmd += "set ThisDir=%~dp0`r`n"
-$startCmd += "%ThisDir%\..\jre\bin\java.exe -jar %ThisDir%\slave.jar -jnlpUrl %JSERVER%/computer/%NAME%/slave-agent.jnlp -secret %SECRET%`r`n"
-$startCmd | Set-Content "$($jenkinsCliLocation)start_slave.cmd"
+    #
+    # Create start_slave.cmd
+    #
+    $startCmd = ""
+    $startCmd += "set NAME=$slaveName`r`n"
+    $startCmd += "set JSERVER=$jenkins`r`n"
+    $startCmd += "set SECRET=$secret`r`n"
+    $startCmd += "set ThisDir=%~dp0`r`n"
+    $startCmd += "%ThisDir%\..\jre\bin\java.exe -jar %ThisDir%\slave.jar -jnlpUrl %JSERVER%/computer/%NAME%/slave-agent.jnlp -secret %SECRET%`r`n"
+    $startCmd | Set-Content "$($jenkinsCliLocation)start_slave.cmd"
 
 }
 
